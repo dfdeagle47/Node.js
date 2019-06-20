a7a586d7fed7c58e4930e349f63c2e71e6e734a9
codelawer <codelawe@gmail.com>
2019-06-21 01:14:34 +0200

    Codelawer Week1 Assignment

---
+++ week1/main.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+fs.open(process.argv[3], 'r', (err, fd) => {
+  if (err) {
+    if (err.code === 'ENOENT') {
+      const data = new Uint8Array(Buffer.from(process.argv[2]));
+      fs.writeFile(process.argv[3], data, err => {
+        if (err) throw err;
+        console.log('The new file has been saved');
+      });
+
+      return;
+    }
+  } else {
+    fs.unlink(process.argv[3], err => {
+      if (err) {
+        throw err;
+      } else {
+        fs.unlink(process.argv[3], (err, data) => {
+          console.log('The file has been deleted');
+        });
+      }
+    });
+  }
+});
+
+// BASH
+
+// if  file exist:
+// $ node main.js 'my text content' './my-output.txt'
+// The file has been deleted
+
+// if  file do not exist:
+// $ node main.js 'my text content' './my-output.txt'
+// The new file has been saved
